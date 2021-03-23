90b57d4f4e8dff0db464cc1146289b2b02176acc
helchall <h_el_chall@hotmail.com>
2021-03-23 17:33:51 +0100

    Adding new entities tag and commen and new relations between entitites

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -3,7 +3,10 @@
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiSubresource;
 use App\Repository\ArticleRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
 
@@ -81,10 +84,27 @@ class Article
      */
     private $createdAt;
 
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Comment", mappedBy="article", orphanRemoval=true)
+     * @ApiSubresource
+     *
+     * @Groups("article:read")
+     */
+    private $comments;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Tag::class, inversedBy="articles", cascade="persist")
+     *
+     * @Groups({"article:read", "article:write"})
+     */
+    private $tags;
+
     public function __construct()
     {
         $this->isPublished = false;
         $this->createdAt   = new \DateTime();
+        $this->comments    = new ArrayCollection();
+        $this->tags        = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -184,6 +204,60 @@ class Article
     public function setCreatedAt(\DateTimeInterface $createdAt): self
     {
         $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Comment[]
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): self
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments[] = $comment;
+            $comment->setArticle($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): self
+    {
+        if ($this->comments->removeElement($comment)) {
+            // set the owning side to null (unless already changed)
+            if ($comment->getArticle() === $this) {
+                $comment->setArticle(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Tag[]
+     */
+    public function getTags(): Collection
+    {
+        return $this->tags;
+    }
+
+    public function addTag(Tag $tag): self
+    {
+        if (!$this->tags->contains($tag)) {
+            $this->tags[] = $tag;
+        }
+
+        return $this;
+    }
+
+    public function removeTag(Tag $tag): self
+    {
+        $this->tags->removeElement($tag);
 
         return $this;
     }
